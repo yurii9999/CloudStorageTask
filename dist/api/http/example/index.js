2e899f5d05b5515ba877059902847d7041cf9f93
ImAllergicToFish <55061691+ImAllergicToFish@users.noreply.github.com>
2022-11-14 18:33:37 +0300

    Delete dist directory

--- dist/api/http/example/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = (app) => {
-    app.get('/example', require('./getExampleMessage').default);
-    app.post('/example/echo', require('./echoMessage').default);
-};
-//# sourceMappingURL=index.js.map
