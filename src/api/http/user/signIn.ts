3efb181cc07235e4d1f2f4f5ff439943e26e211e
Yurii <yusekuzmin@gmail.com>
2023-02-16 09:35:53 +0300

    sign in controller refactored

--- src/api/http/user/signIn.ts
+++ src/api/http/user/signIn.ts
@@ -1,16 +1,16 @@
 import ApiResponseHandler from '../apiResponseHandler';
-import { Request, Response, NextFunction } from 'express';
-import jwt, { Secret, JwtPayload } from 'jsonwebtoken';
-import { AuthData, SignInData } from '../../../ts/types';
+import { Request, Response } from 'express';
+import { SignInData } from '../../../ts/types';
 import UserService from '../../../services/userService';
 
 
 export default async (req: Request, res: Response) => {
     try {
-        if ( !req.body.login || !req.body.password )
+        const signInData: SignInData = req.body
+
+        if ( !signInData.login || !signInData.password )
             return ApiResponseHandler.messageResponse(req, res, "You need login and password to sign in", 400)
         
-        const signInData: SignInData = { login: req.body.login, password: req.body.password }
         return ApiResponseHandler.success(req, res, await UserService.signInUser(signInData))
     } catch (error) {
         await ApiResponseHandler.error(req, res, error);
